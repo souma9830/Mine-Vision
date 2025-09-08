148e7a0e784a37f41f3eb9f2d26f2bb70e0a4ffd
souma9830 <prabirkumarshee8@gmail.com>
2025-09-08 22:50:13 +0530

    Reaminng part Complete

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -3,24 +3,26 @@ import { FaArrowRight } from "react-icons/fa";
 function Hero(){
     return(
         <>
-            <main className="w-full h-screen mt-[14vh] flex flex-col items-center px-2  gap-[2rem] text-center">
-                <h1 className="text-4xl sm:text-6xl md:text-8xl mt-[8rem]">AI Anemia Detection</h1>
-                <h2 className="text-2xl tracking-wide text-gray-700 sm:text-4xl md:text-5xl">Fast, affordable, and camera-based anemia screening.</h2>
+            <main className="w-full h-screen mt-[24vh] flex flex-col items-center px-2 gap-[2rem] text-center">
+               
+                <div className="w-full max-w-6xl rounded-3xl bg-[#DAEBA7] p-8 md:p-12 flex flex-col items-center gap-8 top-8">
+                    <h1 className="mt-8 text-4xl font-bold sm:text-6xl md:text-8xl">Mine Vision.<span className="text-green-400">AI</span></h1>
+                    <h2 className="text-2xl font-semibold tracking-wide text-gray-700 sm:text-3xl">-Predeictive Intelligence For save Mines</h2>
+                    <h2 className="text-3xl italic font-semibold">AI-Powered Rockfall prediction System For open Pit Minds <br /> Helping You Prevent risk before they become Disaster </h2>
 
-                <div className="flex gap-5 mt-3">
-                    <button className='flex items-center gap-2 px-6 py-2 font-semibold text-gray-900 transition-all bg-blue-400 rounded-2xl hover:bg-blue-600 ease group'>
-                            Watch Demo
-                            <span className='transition-transform duration-300 group-hover:translate-x-3'>
-                                    <FaArrowRight />
-                            </span>
-                    </button>
-                    <button className='flex items-center gap-2 px-6 py-2 font-semibold text-blue-500 transition-all bg-transparent border-2 border-blue-400 hover:text-blue-600 rounded-2xl hover:border-blue-600 ease group'>
-                            Dashboard
-                            <span className='transition-transform duration-300 group-hover:translate-x-3'>
-                                <FaArrowRight />
-                            </span>
-                    </button>
+                    <div className="flex gap-5 mt-3">
+                        <button className='flex items-center gap-2 px-6 py-2 font-semibold text-white transition-all bg-black rounded-2xl hover:bg-gray-700 ease group'>
+                                Watch Demo
+                               
+                        </button>
+                        <button className='flex items-center gap-2 px-6 py-2 font-semibold text-white transition-all bg-transparent bg-black border-2 hover:text-blue-600 rounded-2xl hover:border-blue-600 ease group'>
+                                Dashboard
+                               
+                        </button>
+                    </div>
                 </div>
+                
+               
             </main>
         </>
     )
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -12,25 +12,22 @@ function Navbar(){
             <nav 
                 className="fixed left-1/2 top-4 -translate-x-1/2 w-[86%] h-[13vh] z-40
                 rounded-[2.5rem] 
-                bg-white/20 
-                backdrop-blur-xl 
+                bg-white
                 border border-white/40 
                 shadow-[0_8px_32px_0_rgba(0,0,0,0.1)] 
                 flex items-center justify-between md:px-8 px-2">
-                <div className='w-[7rem] h-[7rem]'>
+                <div className='w-[4rem] h-[4rem]'>
                     <img src={Logo} alt="Logo"  className='w-full h-full' />
                 </div>
                 <ul className='hidden text-xl font-semibold gap-7 md:flex justify-evenly'>
                     <li className='transition-all cursor-pointer hover:text-blue-400 ease'>Home</li>
-                    <li className='transition-all cursor-pointer hover:text-blue-400 ease'>About</li>
+                    <li className='transition-all cursor-pointer hover:text-blue-400 ease'>Feature</li>
                     <li className='transition-all cursor-pointer hover:text-blue-400 ease'>Conatct</li>
                 </ul>
 
-                <button onClick={()=>setIspoped(!isPoped)} className='items-center hidden gap-2 px-6 py-2 font-semibold text-gray-900 transition-all bg-blue-400 md:flex rounded-2xl hover:bg-blue-600 ease group'>
+                <button onClick={()=>setIspoped(!isPoped)} className='items-center hidden gap-2 px-6 py-2 font-semibold text-white transition-all bg-emerald-500 md:flex rounded-2xl hover:bg-blue-600 ease group'>
                     SignUp/SignIn 
-                    <span className='transition-transform duration-300 group-hover:translate-x-3'>
-                        <FaArrowRight />
-                    </span>
+                    
                 </button>
 
                 <button onClick={()=>setIsOpen(true)}  className='relative mr-4 text-4xl md:hidden'>{isOpen?<RxCross1 />:<RiMenu3Fill />}</button>
@@ -42,7 +39,7 @@ function Navbar(){
                     <li className='mt-5 transition-all cursor-pointer hover:text-blue-400 ease'>Home</li>
                     <li className='transition-all cursor-pointer hover:text-blue-400 ease'>About</li>
                     <li className='transition-all cursor-pointer hover:text-blue-400 ease'>Conatct</li>
-                    <button onClick={()=>setIspoped(true)} className='flex items-center gap-2 px-6 py-2 font-semibold text-gray-900 transition-all bg-blue-400 rounded-2xl hover:bg-blue-600 ease group'>
+                    <button onClick={()=>setIspoped(true)} className='flex items-center gap-2 px-6 py-2 font-semibold text-gray-900 transition-all bg-blue-400 rounded-2xl hover:bg-gray-700 ease group'>
                         SignUp/SignIn 
                         <span className='transition-transform duration-300 group-hover:translate-x-3'>
                             <FaArrowRight />
